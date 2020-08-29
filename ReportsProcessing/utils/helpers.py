ef0755b5dede452a6abde590a596012adc58156a
guest512 <denis.legashov@gmail.com>
2020-08-29 20:51:05 +0300

    small update for reports processing notebook

--- ReportsProcessing/utils/helpers.py
+++ ReportsProcessing/utils/helpers.py
@@ -1,3 +1,6 @@
+import time as _time
+import locale as _locale
+import contextlib as _ctxlib
 import pandas as _pd
 
 
@@ -5,3 +8,9 @@ def normalize(values: _pd.Series) -> _pd.Series:
     ''' Maps series of values to scale from 0.0 to 1.0 and returns it as a new Series object '''
 
     return values / values.max()
+
+@_ctxlib.contextmanager
+def setlocale_ctx(locale:str):
+    saved = _locale.setlocale(_locale.LC_ALL)
+    yield _locale.setlocale(_locale.LC_ALL, locale)
+    _locale.setlocale(_locale.LC_ALL, saved)
