4c7a82990ad7a0223035a97b3cc6b27b44c941f9
guest512 <denis.legashov@gmail.com>
2020-09-29 16:43:39 +0300

    Added throwing errors if image can't be built

--- build.ps1
+++ build.ps1
@@ -3,16 +3,31 @@ param (
     [switch] $RunOnly
 )
 
-function Log-String ($stringToWrite) {
+function Write-Log-String ($stringToWrite) {
     Write-Host $stringToWrite -ForegroundColor DarkBlue
 }
 
-function Stop-And-Remove-Previous-Container ($container_name)  {
-    Log-String("Looking for existing containers from previous runs")
+function Write-Log-Error ($stringToWrite) {
+    Write-Host $stringToWrite -ForegroundColor DarkRed  
+}
+
+function Confirm-Last-Result ($res) {
+    if ($res) {
+        Write-Log-String("SUCCESS")
+    }
+    
+    if (!$res) {
+        Write-Log-Error("FAIL")
+        throw "Last command failed."
+    }
+}
+
+function Stop-And-Remove-Previous-Container ($container_name) {
+    Write-Log-String("Looking for existing containers from previous runs")
     $containers = docker ps -af "name=$container_name" --format "{{.ID}} {{.Names}}" | ConvertFrom-String -PropertyNames ID, Name
 
     if ($containers.Name -and $containers.Name.Contains($container_name)) {
-        Log-String("Stopping and removing container")
+        Write-Log-String("Stopping and removing container")
         docker stop $container_name
         docker rm $container_name
     }
@@ -22,55 +37,66 @@ function Stop-And-Remove-Previous-Container ($container_name)  {
 function Build-Docker {
     $today = Get-Date -Format "yy.MM.dd"
 
-    Log-String("Build docker images...")
+    Write-Log-String("Build docker images...")
 
-    Log-String("Data preparation image...")
+    Write-Log-String("Data preparation image...")
     docker build --target data_preparation -t covid_sandbox_prepare:$today -t covid_sandbox_prepare:latest .
-    Log-String("Reports generator image...")
+    Confirm-Last-Result $?
+    
+    Write-Log-String("Reports generator image...")
     docker build --target reports_generator -t covid_sandbox_generator:$today -t covid_sandbox_generator:latest .
-    Log-String("Reports processing image...")
+    Confirm-Last-Result $?
+
+    Write-Log-String("Reports processing image...")
     docker build --target reports_processor -t covid_sandbox_processing:$today -t covid_sandbox_processing:latest .
+    Confirm-Last-Result $?
 }
 
 function Start-Docker {
-    Log-String("Prepare data ...")
+    Write-Log-String("Prepare data ...")
     docker run -v ${pwd}/Data:/work/Data -v ${pwd}/Data/temp:/work/bin/Release/Data --name covid_sandbox_prepare_afd876 --rm covid_sandbox_prepare
 
-    Log-String("Generate reports...")
+    Write-Log-String("Generate reports...")
     docker run -v ${pwd}/Data/temp:/work/Data -v ${pwd}/ReportsProcessing/reports:/work/reports --name covid_sandbox_generator_afd876 --rm covid_sandbox_generator
 
-    Log-String("Run processing reports image ...")
+    Write-Log-String("Run processing reports image ...")
     Stop-And-Remove-Previous-Container("covid_sandbox_processing_afd876")
     docker run -d -p 8888:8888 -v ${pwd}/ReportsProcessing:/work --name covid_sandbox_processing_afd876 covid_sandbox_processing
     Start-Sleep 1
-    Log-String("Image started")
+    Write-Log-String("Image started")
     
-    Write-Host (docker logs covid_sandbox_processing_afd876)
+    Write-Log-String (docker logs covid_sandbox_processing_afd876)
 }
 
 function Build-Local {
-    Log-String("Build util for reports conversion")
+    Write-Log-String("Build util for reports conversion")
     dotnet.exe msbuild .\build\build.proj /t:"Build;PrepareData" /p:Configuration=Release
-    dotnet.exe test --no-build -c:Release
+    Confirm-Last-Result $?
 
-    Log-String("Run util to convert reports")
+    dotnet.exe test --no-build -c:Release
+    Confirm-Last-Result $?
+
+    Write-Log-String("Run util to convert reports")
     Set-Location .\bin\Release
     dotnet.exe .\CovidSandbox.dll
+    Confirm-Last-Result $?
+
     Set-Location ..\..
 
-    Log-String("Copy reports to reports processing folder")
+    Write-Log-String("Copy reports to reports processing folder")
     New-Item .\ReportsProcessing\ -Name "reports" -ItemType "directory" -Force
     Copy-Item .\bin\Release\reports .\ReportsProcessing -Recurse -Force
 }
 
 if ($Docker) {
-    if(!$RunOnly) {
+    if (!$RunOnly) {
         Build-Docker
     }
-    Log-String("Remove previous reports...")
+
+    Write-Log-String("Remove previous reports...")
     Remove-Item .\ReportsProcessing\reports -Recurse -Force
     Start-Docker
-    Log-String("Clean temp data...")
+    Write-Log-String("Clean temp data...")
     Remove-Item .\Data\temp -Recurse -Force
 }
 else {
