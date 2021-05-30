afef67db8544d6736332d164a7bda84b54c99532
guest512 <denis.legashov@gmail.com>
2021-05-30 18:49:58 +0300

    Minor cleanup

--- Tools/src/ReportsGenerator/ReportsGenerator/Data/DataSources/JHopkinsDataSourceReader.cs
+++ Tools/src/ReportsGenerator/ReportsGenerator/Data/DataSources/JHopkinsDataSourceReader.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using ReportsGenerator.Data.Providers;
-using ReportsGenerator.Utils;
 
 namespace ReportsGenerator.Data.DataSources
 {
